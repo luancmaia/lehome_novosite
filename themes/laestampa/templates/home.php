2cb373402503e81430b81c81c549c90ed12e5001
Ernandes Ferreira <ernandes.ferreira@laestampa.dom>
2017-11-27 18:18:52 -0200

    atualizando home

--- themes/laestampa/templates/home.php
+++ themes/laestampa/templates/home.php
@@ -25,14 +25,92 @@ $imagem_banner2 = get_template_directory_uri().'/assets/images/banner2.jpg';
 	<div class="row">
 		<div class="col-12">
 			<div class="chamada-home text-center">
-				<h2> Nisl viverra eros diam nulla elementum, fringilla fermentum donec cubilia a, libero habitasse interdum torquent.</h2>
+				<h2> Estimulamos novas formas de expressão e comportamento na decoração.</h2>
 			</div>
 		</div>
 	</div>
 </div>	
 <div class="container-fluid">
-	<div class="row row-bottom-separete">
-			teste
+	<div class="row rowBtn-compra-home">
+			<div class="col-12 col-md-6 noPadding-left">
+				<a href="#" class="box_bannerBtn" title="Compre Agora">
+					<div class="col-estampados">						
+						<div class="title-banner">
+							<h3>Tecidos <br> Estampados </h3>
+							<div class="btnBanner-comprar">
+								Compre agora
+							</div>
+						</div>							
+					</div>
+				</a>
+			</div>
+			<div class="col-12 col-md-6 noPadding-right">
+				<a href="#" class="box_bannerBtn" title="Compre Agora">
+					<div class="col-papel">						
+							<div class="title-banner">
+								<h3>Papel <br> de Parede </h3>
+								<div class="btnBanner-comprar">
+									Compre agora
+								</div>
+							</div>							
+					</div>
+				</a>
+			</div>
+	</div>
+</div>
+
+<div class="container-fluid primeira_linha">
+	<div class="row rowLine1-desenhos">
+			<div class="col-12 col-md-6 noPadding-left">
+				<div class="item_categoriaHome">
+				<a href="#" class="line_linkBox" title="Categoria Estampados">
+					<div class="mascara_banner">
+						<h3 class="text-center"> Tecidos <br/> Estampados </h3>
+					</div>
+				</a>
+					<img src="<?php echo get_template_directory_uri().'/assets/images/home/line1_colun1.jpg' ?>" alt="" />
+				</div>	
+			</div>
+			<div class="col-12 col-md-6 noPadding-right">
+				<div class="item_categoriaHome">
+					<a href="#" class="line_linkBox" title="Categoria Estampados">
+						<div class="mascara_banner">
+							<h3 class="text-center"> Papel <br/> de Parede </h3>
+						</div>
+					</a>
+						<img src="<?php echo get_template_directory_uri().'/assets/images/home/line1_colun2.jpg' ?>" alt="" />
+				</div>
+				<div class="item_categoriaHome">
+					<h4>
+						Estimulamos novas formas de <strong>expressão e comportamento</strong> na decoração. <strong>Estimulamos</strong> novas formas <strong>de decoração</strong>.
+					</h4>
+				</div>
+			</div>
+	</div>
+</div>
+
+<div class="container-fluid segunda_linha">
+	<div class="row rowLine1-desenhos">
+			<div class="col-12 col-md-6 noPadding-left">
+				<div class="item_categoriaHome">
+				<a href="#" class="line_linkBox" title="Categoria Estampados">
+					<div class="mascara_banner">
+						<h3 class="text-center"> Tecidos <br/> Geométricos </h3>
+					</div>
+				</a>
+					<img src="<?php echo get_template_directory_uri().'/assets/images/home/line2_colun1.jpg' ?>" alt="" />
+				</div>	
+			</div>
+			<div class="col-12 col-md-6 noPadding-right">
+				<div class="item_categoriaHome">
+					<a href="#" class="line_linkBox" title="Categoria Estampados">
+						<div class="mascara_banner">
+							<h3 class="text-center"> Papel de Parede <br/> Estampados </h3>
+						</div>
+					</a>
+						<img src="<?php echo get_template_directory_uri().'/assets/images/home/line2_colun2.jpg' ?>" alt="" />
+				</div>
+			</div>
 	</div>
 </div>
 
