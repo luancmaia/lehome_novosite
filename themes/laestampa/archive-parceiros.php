81d610ee669cdfaf8164556d34cc41ea5301e38d
admin <admin@MAC21.local>
2017-12-15 12:58:53 -0200

    finalizando pag sobre

--- themes/laestampa/archive-parceiros.php
+++ themes/laestampa/archive-parceiros.php
@@ -34,6 +34,8 @@ get_header();
 			        $numOrder = get_field('ordem_logo', $parceiro->ID);
 			        $imagens = get_field('galeria_parceiros', $parceiro->ID);
 			        $content = $parceiro->post_content;
+
+			        var_dump($parceiro);
 		?>				        
 			        <div class="col-12 col-md-6">
 				        <div class="gridItem-parceiros" id="<?php echo $parceiro->ID; ?>" data-content='<?php echo json_encode( $content );?>' data-imagens='<?php echo json_encode( $imagens ); ?>' data-toggle="modal" data-target=".bd-example-modal-lg">
