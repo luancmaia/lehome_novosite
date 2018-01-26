f5a962653aef96c7bb52d8d07b4ed9694bb5478c
admin <admin@MAC21.local>
2018-01-26 16:54:12 -0200

    imagem colecao descer um pouco

--- themes/laestampa/hooks-custom/hooks.php
+++ themes/laestampa/hooks-custom/hooks.php
@@ -24,7 +24,7 @@ function remove_order_product(){
 }
 add_action('init', 'remove_order_product');
 
-remove_action( 'woocommerce_before_shop_loop', 'woocommerce_pagination', 30 );
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_pagination', 10 );
 
 
 
