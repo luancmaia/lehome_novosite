2c1aa2c00938971f7b8d2e76f0612a67dad4656d
ernandes NA <ernandes.dev@gmail.com>
2018-02-01 13:58:53 -0200

    resolvendo problema checkout

--- themes/laestampa/hooks-custom/theme-functions.php
+++ themes/laestampa/hooks-custom/theme-functions.php
@@ -1,20 +1,23 @@
 <?php
 
-add_filter( 'wcbcf_billing_fields', 'function_doido' );
+// add_filter( 'wcbcf_billing_fields', 'function_doido' );
+// function function_doido( $new_fields ) {
+// 	$tipo = $new_fields['billing_persontype'];
+// 	$user_tipo = current_user();
 
-function function_doido( $new_fields ) {
-	$tipo = $new_fields['billing_persontype'];
+// 	if( $user_tipo == "pj" ){
+// 		$aux = $new_fields['billing_persontype']['options'][1];
+
+// 		$new_fields['billing_persontype']['options'][1] = $new_fields['billing_persontype']['options'][2];
+
+
+
+// 		$new_fields['billing_persontype']['options'][2] = $aux;
+
 	
-
-	$current_user = current_user();
-
-	if( $current_user == "pj" ){
-		$aux = $new_fields['billing_persontype']['options'][1];
-		$new_fields['billing_persontype']['options'][1] = $new_fields['billing_persontype']['options'][2];
-		$new_fields['billing_persontype']['options'][2] = $aux;
-	}
-	return $new_fields;
-}
+// 	}
+// 	return $new_fields;
+// }
 
 //functions tema
 function banner_page($page_id){
